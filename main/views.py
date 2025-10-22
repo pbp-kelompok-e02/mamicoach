e428c937784c320ae470e17cef7b046c07b7e342
Kevin Cornellius Widjaja <kevinbananax@gmail.com>
2025-10-22 20:00:56 +0700

    feat: Update landing page structure and styles with new hero and coach sections (#4)

--- main/views.py
+++ main/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def show_main(request):
-    return render(request, "pages/main.html")
+    return render(request, "pages/landing_page/index.html")
