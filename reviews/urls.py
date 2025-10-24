6c2e7a6fca25de9573a513800d3829181647167e
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2025-10-24 18:44:56 +0700

    feat: integrate chat, review, and dashboard

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create_review, edit_review, show_sample_review
+from .views import create_review, edit_review, delete_review, show_sample_review
 
 app_name = "reviews"
 
 urlpatterns = [
     path("review/create/<int:booking_id>", create_review, name="create_review"),
     path("review/edit/<int:review_id>", edit_review, name="edit_review"),
+    path("review/delete/<int:review_id>", delete_review, name="delete_review"),
 ]
